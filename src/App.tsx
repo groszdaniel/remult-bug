ef1a1ca0cb83f59de45589a0ddab778b2abe3737
Dániel Grósz <groszdanielpub@gmail.com>
2024-01-21 07:41:54 +0100

    backendMethod

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { remult } from "remult";
+import { Foo } from "./shared/Foo";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [result, setResult] = useState(0);
+  const [foos, setFoos] = useState<Foo[]>([]);
+  useEffect(() => {
+    // Make sure there are at least two foos
+    (async () => {
+      if (foos.length < 2) {
+        const _foos = await remult.repo(Foo).find();
+        for (let i = _foos.length; i < 2; ++i) {
+          const foo = remult.repo(Foo).create({ a: 1 });
+          await foo.save();
+          _foos.push(foo);
+        }
+        setFoos(_foos);
+      }
+    })();
+  }, []);
+
+  async function onClick() {
+    /*
+     * If the parameter is passed as an object, the result will be 101. If it's passed as an id,
+     * the changed field does not get passed to the server, so the result will be 2.
+     * 
+     * With the react-vite-express-server setup, if paramTypes is not specified, it gets passed
+     * as an object (even if the line below is commented out). */
+    foos[1].a = 100;
+
+    const _result = await foos[0].backendMethod(foos[1]);
+    setResult(_result);
+  }
 
   return (
     <div className="App">
@@ -17,9 +47,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={onClick}>count is {result}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -28,7 +56,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/shared/Foo.ts
+++ src/shared/Foo.ts
@@ -1,12 +1,16 @@
-import { Entity, Fields } from "remult";
+import { BackendMethod, Entity, EntityBase, Fields } from "remult";
 
 @Entity("foo", {
   allowApiCrud: true,
 })
-export class Foo {
+export class Foo extends EntityBase {
   @Fields.cuid()
   id = "";
 
   @Fields.integer()
   a = 0;
+
+  @BackendMethod({ allowed: true/*, paramTypes: [Foo]*/ }) async backendMethod(bar: Foo) {
+    return this.a + bar.a;
+  }
 }
